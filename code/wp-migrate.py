2c156d144bd8c4eba706a67669f959443e13a1ec
S. Joshua Swamidass <swamidass@gmail.com>
2021-02-04 13:00:21 -0600

    moved name to title in taxonomy terms.

--- code/wp-migrate.py
+++ code/wp-migrate.py
@@ -3,28 +3,36 @@
 import json
 import pathlib
 
-T = json.load(open("wp/tags.json"))
+def load(f):
+  return dict((j["id"], j) for j in json.load(open(f)))
+
 
 def tags():
-  for t in T:
-    path = "content/tags/%s/" % t["slug"]
+  for t in T.values():
+    path = "content/categories/%s/" % t["slug"]
     p = pathlib.Path(path)
     p.mkdir(parents=True, exist_ok=True)
 
     with open(path + "_index.md", 'w') as f:
-      f.write("---\nname: %s\n---" %t["name"])   
+      f.write("---\ntitle: %s\n---" %t["name"])   
 
-
-A = json.load(open("wp/authors.json"))
 
 def authors():
-  for a in A:
+  for a in A.values():
     path = "content/authors/%s/" % a["slug"]
     p = pathlib.Path(path)
     p.mkdir(parents=True, exist_ok=True)
 
     with open(path + "_index.md", 'w') as f:
-      f.write("---\nname: %s\n---\n%s" %(a["name"], a["description"]))
+      f.write("---\ntitle: %s\n---\n%s" %(a["name"], a["description"]))
 
 
+T = load("wp/tags.json")
+A = load("wp/authors.json")
+C = load("wp/categories.json")
+P = load("wp/posts.json")
+
+
+
+tags()
 authors()
