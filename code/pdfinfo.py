9da2eb06676bbdcc99431a5530d1d1e272625149
S. Joshua Swamidass <swamidass@gmail.com>
2021-09-07 23:51:12 -0500

    fixed bug in lastmod date.

--- code/pdfinfo.py
+++ code/pdfinfo.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 OUT = [] 
 for g in glob.glob("static/pdf/**/*.pdf", recursive=True): 
    url = g[6:]
-   lastmod = check_output(["git", "log", "-1", '--pretty=%cI']).decode('utf-8').strip()
+   lastmod = check_output(["git", "log", "-1", '--pretty=%cI', g]).decode('utf-8').strip()
    OUT.append( {"File": url, "Lastmod": lastmod} )
 
 
