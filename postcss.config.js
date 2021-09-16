4550e45cc570aff6394c6ee14a889d83a4264fbc
S. Joshua Swamidass <swamidass@gmail.com>
2021-09-16 13:03:12 -0500

    inlined font awesome

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
 var plugins = [];
 const purgecss = require('@fullhuman/postcss-purgecss')
+const importUrl = require('postcss-import-url');
 
 
-plugins.push(require("autoprefixer"));
-
+plugins.push(require("postcss-import-url")({"resolveUrls": true}));
 
 plugins.push(purgecss( { fontFace: false, content: [ 'layouts/**/*.html', 'assets/css/main.scss' ], }))
 
+plugins.push(require("autoprefixer"));
 
 plugins.push(require("cssnano")({ preset: 'default', }));
 
+plugins.push(require("postcss-prettify"));
 
 module.exports = {    
   plugins: plugins 
