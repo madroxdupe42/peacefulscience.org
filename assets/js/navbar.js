c8159fd0ee0f3db23f1e0c897d4ce9c7566f3323
S. Joshua Swamidass <swamidass@gmail.com>
2021-09-24 13:43:34 -0400

    refactored javascript for modularization, minification, fingerprinting, longer cache time, and hugo asset pipeline

---
+++ assets/js/navbar.js
@@ -0,0 +1,98 @@
+
+
+let navbarToggler = document.querySelector('.navbar-toggler');
+let navbarDropdown = document.querySelector('#navbarNavDropdown');
+let navbarDropdownExpanded = navbarDropdown.getAttribute('aria-expanded');
+
+// Mobile menu toggle
+
+navbarToggler.addEventListener('click', function(){
+  navbarDropdown.classList.toggle('show');
+
+  if(navbarDropdownExpanded == "true") {
+    navbarDropdownExpanded = "false";
+  } else {
+    navbarDropdownExpanded = "true";
+  }
+
+  navbarDropdown.setAttribute("aria-expanded", navbarDropdownExpanded);
+});
+
+function closeNavbar(){
+	  navbarDropdown.classList.remove('show');
+	  navbarDropdownExpanded = "false";
+	  navbarDropdown.setAttribute("aria-expanded", navbarDropdownExpanded);
+}	
+	
+// Dropdown toggle
+
+function getTogglerId(className, event, fn) {
+  let list = document.querySelectorAll(className);
+  for (let i = 0, len = list.length; i < len; i++) {
+      list[i].addEventListener(event, fn, false);
+  }
+}
+
+getTogglerId('.dropdown-toggle', 'click', toggleDropdown);
+
+let dropdownMenus = document.querySelectorAll('.dropdown-menu');
+let dropdownTogglers = document.querySelectorAll('.dropdown-toggle');
+
+function closeMenus(){
+  for (let j = 0; j < dropdownMenus.length; j++) {
+    dropdownMenus[j].classList.remove('show');
+  }
+  for (let k = 0; k < dropdownTogglers.length; k++) {
+    dropdownTogglers[k].classList.remove('show');
+    dropdownTogglers[k].setAttribute('aria-expanded','false');
+  }
+}
+
+function toggleDropdown(e) {
+  let isOpen = this.classList.contains('show');
+
+  if (!isOpen) {
+    closeMenus();
+    document.querySelector(`[aria-labelledby=${this.id}]`).classList.add('show');
+    this.classList.add('show');
+    this.setAttribute('aria-expanded','true');
+  } else if (isOpen) {
+    closeMenus();
+  }
+
+  e.preventDefault();
+}
+
+// Close dropdowns on focusout
+
+let navbar = document.querySelector('.navbar');
+
+navbar.addEventListener('focusout', function() {
+
+  window.onclick = function(event){
+    if (document.querySelector('.navbar').contains(event.target)){
+      return;
+    } else{
+      closeMenus();
+    }
+  };
+});
+
+
+
+var prevScrollpos = window.pageYOffset;
+let navBar = document.getElementById("navbar")
+
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos || currentScrollPos < 80) {
+	  navBar.style.top = "0";
+	  closeMenus();
+  } else {
+	  closeMenus();
+	  navBar.style.top =  (-navBar.offsetHeight).toString() + "px" ;  
+  }
+  prevScrollpos = currentScrollPos; 
+}
+
