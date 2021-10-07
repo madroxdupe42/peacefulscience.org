059d860c9645915c49217c7a337584eadeb65167
S. Joshua Swamidass <swamidass@gmail.com>
2021-10-07 10:06:44 -0500

    added some unused postcss modules for later

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,9 @@ var plugins = [];
 const purgecss = require('@fullhuman/postcss-purgecss')
 const importUrl = require('postcss-import-url');
 
-
 plugins.push(require("postcss-import-url")({"resolveUrls": true}));
+
+// plugins.push(require('postcss-amp'));
 
 plugins.push(purgecss( { fontFace: false, content: [ 'layouts/**/*.html', 'assets/css/main.scss' ], }))
 
