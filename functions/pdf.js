0818d930439511347ae4a6dd73224b4a849953e4
S. Joshua Swamidass <swamidass@gmail.com>
2021-12-08 01:01:52 -0600

    added commandline prince for testing

--- functions/pdf.js
+++ functions/pdf.js
@@ -1,11 +1,14 @@
 require("dotenv").config() 
 const axios = require('axios');
 const urljoin = require('url-join');
+const util   = require("util");
+var os = require('os');
+var fs = require('fs');
+const Prince = require('prince');
 
 const baseurl = "https://peacefulscience.org";
 
-exports.handler =  async function(event, context) {
-    const url = urljoin(baseurl, event.queryStringParameters.url);
+async function docraptor(url) {
     const docraptor = "https://" + process.env.DOCRAPTOR + "@docraptor.com/docs";
     const params = {"test": true,
            "document_url": url,
@@ -17,7 +20,7 @@ exports.handler =  async function(event, context) {
     let response = await axios.post( docraptor, params, {
       responseType: 'arraybuffer'
     });
-
+    
     return {
       isBase64Encoded: true,
       statusCode: response.status,
@@ -27,5 +30,43 @@ exports.handler =  async function(event, context) {
         "x-frame-options": 'SAMEORIGIN',
         "x-permitted-cross-domain-policies": 'none',
       }
-    }
+    }    
+}
+
+async function prince(url) {
+  tfile = os.tmpdir()+"output.pdf";
+  
+  return await Prince()
+    .inputs(url)
+    .output(tfile)
+    .execute()
+    .then(function () {
+      return fs.readFileSync(tfile, null);
+    })
+    .then(function (buffer) {    
+     return {    
+      statusCode: 200,
+      isBase64Encoded: true,
+      body: buffer.toString('base64') ,
+      headers: {
+        "x-frame-options": 'SAMEORIGIN',
+        "x-permitted-cross-domain-policies": 'none',
+      }
+    }         
+        
+    }, function (error) {
+      return {
+        statusCode: 500,
+        body: util.inspect(error)
+      }
+    })
+  
+}
+
+exports.handler =  async function(event, context) {
+
+    let url = urljoin(baseurl, event.path.split("/").slice(2).join("/"));
+
+    return prince(url);
+
 }
