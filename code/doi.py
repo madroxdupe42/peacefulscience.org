40d7056783d89488a50f3126ae148e631f5f35e6
S. Joshua Swamidass <swamidass@gmail.com>
2021-12-11 00:38:37 -0600

    added DOI assigment code and crossref.xml template

---
+++ code/doi.py
@@ -0,0 +1,39 @@
+import json
+import random
+import sys
+
+
+
+doiprefix="10.54739"
+CHAR = "abcdeghijkmopqrtuvwxy12346789"
+
+DOI = json.load(open("data/doi.json"))
+
+dois = set(DOI.values())
+
+def randomchars(n):
+  return "".join(random.choice(CHAR) for i in range(n))
+
+def randomdoi():
+  return doiprefix + "/" + randomchars(4)
+
+def newdoi():
+  while True:
+    d = randomdoi()
+    if d not in dois: break
+  return d
+
+
+
+for rel in sys.argv[1:]:
+  if rel in DOI:
+    print("%s already assigned DOI" % rel)
+  else:
+    DOI[rel] = newdoi()
+    print("%s assigned %s" %( DOI[rel], rel ))
+
+
+OUT = json.dumps(DOI, sort_keys=True, indent=4)
+print(OUT)
+
+print(OUT, file=open("data/doi.json", "w"))  
