daedecdd92e2159f8807594f2b01eda67f472de4
S. Joshua Swamidass <swamidass@gmail.com>
2021-12-12 18:14:28 -0600

    added smart caching to PDF; and limited PDF to article sections

--- functions/pdf/index.js
+++ functions/pdf/index.js
@@ -33,7 +33,7 @@ async function docraptor(url) {
     }    
 }
 
-async function prince(url, name, canurl) {
+async function prince(url) {
   let tfile = os.tmpdir()+"/output.pdf";
   let p = Prince();
   
@@ -55,33 +55,67 @@ async function prince(url, name, canurl) {
       statusCode: 200,
       isBase64Encoded: true,
       body: buffer.toString('base64') ,
-      headers: {
-        "x-frame-options": 'SAMEORIGIN',
-        "x-permitted-cross-domain-policies": 'none',
-        "Content-Disposition": `attachment, filename="${name}"`,
-        "Cache-Control": "max-age=86400, stale-while-revalidate=86400" ,// 0 days, 7 days
-        "Link": `<${canurl}>; rel="canonical"`
-      }
     }         
         
-    }, function (error) {
-      return {
-        statusCode: 500,
-        body: util.inspect(error)
-      }
     }) 
 }
 
+
 exports.handler =  async function(event, context) {
-    let route = event.path.toLowerCase().split("/");
-    let path = route.slice(route.indexOf("pdf")+1, -1);
-        
+    let route = event.path.toLowerCase();
+    route = route.endsWith('/') ? route.slice(0, -1) : route;
+    route = route.split('/');
+    
+    let path = route.slice(route.indexOf("pdf")+1);    
     let url = urljoin(baseurl, "_prince", path.join("/"));
     let canurl = urljoin(baseurl, path.join("/"));
     let title = path.slice(-1)[0];
     let section = path.slice(-2)[0];
     let name = `PS${section}-${title}.pdf`
+    var etag = null;
     
-    return prince(url, name, canurl);
-
+    console.log(url);
+    console.log(canurl);
+    console.log(section);
+    
+    if (! ["articles", "about", "prints"].includes(section) ) 
+      return {statusCode: 404}
+    
+    var headers = {};
+    if ("if-none-match" in event.headers) {
+      etag = event.headers["if-none-match"];
+      headers["If-None-Match"] = etag;
+    }
+      
+    return axios.get(url, headers=headers)
+      .then(res => {    
+        if (res.status === 304 || res.headers['etag'] ===  headers["If-None-Match"] ) 
+          return {
+            statusCode: 304,
+            body: "OK",
+            headers: {"Etag": etag}
+          }
+        etag = res.headers["etag"]
+        
+        return prince(url)
+          .catch( error => {return {statusCode: 500, body: util.inspect(error)}})
+          .then(response => {
+            if (response.statusCode === 200)
+              response["headers"] = {
+                "x-frame-options": 'SAMEORIGIN',
+                "x-permitted-cross-domain-policies": 'none',
+                "Content-Disposition": `attachment, filename="${name}"`,
+                "Cache-Control": "public, max-age=0, must-revalidate",
+                "Link": `<${canurl}>; rel="canonical"`,
+                "Etag": etag
+               }
+            return response;
+           })
+      })
+      .catch (error => {return {statusCode: error.response.status }})      
 }
+     
+    
+    
+
+     
