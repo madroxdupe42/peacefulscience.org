cf80944a81f49dff44f6f06c41c62c599a02fc2c
S. Joshua Swamidass <swamidass@gmail.com>
2021-12-20 12:34:05 -0600

    fixed video width on mobile; tightened video JS to improve SEO

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,32 +1,29 @@
 import YouTubePlayer from 'youtube-player';
 
+
+
 var player = false;
 
 window.PlayVideo = function (yid) {
-	
-	player = player || YouTubePlayer('video-frame');
-	
-
-	var f = document.getElementById('video-frame');
-	f.parentElement.classList.add("active");
-	f.parentElement.classList.remove("shrink");
-	player.loadVideoById(yid);
-	player.playVideo();
+	player = player || YouTubePlayer('video-frame', {playerVars: {modestbranding: 1, playsinline: 1, origin: "https://peacefulscience.org", widget_referrer: window.location}});
+	var video_frame = document.getElementById('video-container');
+	var currentID = document.getElementById('video-id');	
+	video_frame.classList.add("active");
+	video_frame.classList.remove("shrink");
+	player.cueVideoById(yid);
+    currentID.innerHTML = yid;
 }
 
 window.CloseVideo = function () {
 	player.stopVideo();
-	var f = document.getElementById('video-frame');
-	f.parentElement.classList.remove("active");
+	document.getElementById('video-container').classList.remove("active");
 }
 
 window.ShrinkVideo = function () {
-	var f = document.getElementById('video-frame');
-	f.parentElement.classList.add("shrink");
+	document.getElementById('video-container').classList.add("shrink");
 }
 
 window.UnshrinkVideo = function () {
-	var f = document.getElementById('video-frame');
-	f.parentElement.classList.remove("shrink");
+	document.getElementById('video-container').classList.remove("shrink");
 }
 
