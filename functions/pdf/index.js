1b4d6cc08bc81c6a90c984cab1506f8db8463777
S. Joshua Swamidass <swamidass@gmail.com>
2021-12-20 19:56:29 -0600

    added redirect to pdf; added pdfs to sitemap

--- functions/pdf/index.js
+++ functions/pdf/index.js
@@ -44,7 +44,7 @@ exports.handler =  async function(event, context) {
     route = route.endsWith('/') ? route.slice(0, -1) : route;
     
     if (! route.endsWith(".pdf") ) 
-	  return {statusCode: 404}
+	  return {statusCode: 301, location: `${route}.pdf`}
 	
     route = route.slice(0, -4);
     route = route.split('/');
