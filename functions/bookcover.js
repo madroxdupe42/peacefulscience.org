34b03b721c89794c1cedd1c9baf88ac456291d2d
S. Joshua Swamidass <swamidass@gmail.com>
2023-01-02 10:26:19 -1000

    added error handling

--- functions/bookcover.js
+++ functions/bookcover.js
@@ -1,32 +1,38 @@
-require("dotenv").config() 
+require("dotenv").config();
 const { builder } = require("@netlify/functions");
-const axios = require('axios');
+const axios = require("axios");
+const { JsonLdProcessor } = require("jsonld");
 
 async function handler(event, context) {
-    console.info(event.path);
-    
-    let amzn_id = event.path.replace(/^.*?bookcover\//, "");
-    let headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    
-    let url = `http://images.amazon.com/images/P/${amzn_id}.LZZZZZZZ.jpg`
-    console.log(url);
-    
-    let imageBase64 = '';
-  
-    imageBase64 = await axios
-      .get(url, { responseType: 'arraybuffer', headers })
-      .then((response) => Buffer.from(response.data, 'binary').toString('base64'))
-//      .catch((e) => reject(e))
-  
+  console.info(event.path);
 
-  return {
-    statusCode: 200,
-    body: imageBase64,
-    isBase64Encoded: true,         
-    headers: {
-      ttl: 60 * 60 * 24 * 7 * 52
-    }
-  }
+  let amzn_id = event.path.replace(/^.*?bookcover\//, "");
+  let headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+  };
+
+  let url = `http://images.amazon.com/images/P/${amzn_id}.LZZZZZZZ.jpg`;
+  console.log(url);
+
+  return await axios
+    .get(url, { responseType: "arraybuffer", headers })
+    .then((response) => Buffer.from(response.data, "binary").toString("base64"))
+    .then((imageBase64) => ({
+      statusCode: 200,
+      body: imageBase64,
+      isBase64Encoded: true,
+      headers: {
+        ttl: 60 * 60 * 24 * 7 * 52,
+      },
+    }))
+    .catch((e) => ({
+      statusCode: 500,
+      body: JSON.stringify(e),
+      headers: {
+        ttl: 0,
+      },
+    }));
 }
 
 exports.handler = builder(handler);
